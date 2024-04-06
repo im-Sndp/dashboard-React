a40eb158b99e43dde32ce0b82fcf54c49319c119
im-Sndp <stalukdar.2003@gmail.com>
2024-04-07 02:20:20 +0530

    dukaan dashboard completed

--- dukaan/src/App.jsx
+++ dukaan/src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import { Appbar } from './components/appbar/Appbar'
-import { Body } from './components/body/Body'
-import { Sidebar } from './components/sidebar/Sidebar'
+import { Sidebar } from './components/sidebar/Sidebar';
+import { Appbar } from './components/appbar/Appbar';
+import { Body } from './components/body/Body';
+import { Graybackground } from './components/Graybackground';
+
 
 function App() {
 
-  const [isClick,setClick]  = useState(false);
-
-
-  console.log(isClick);
+  const [click,setClick] = useState(false);
 
   return (
-    <div className='flex'>
-      <Sidebar isClick={isClick} />
-      <div>
-        <Appbar setClick ={setClick} />
+    <div className='flex relative'>
+      <Sidebar click={click} />
+      {click && <Graybackground setClick={setClick}/> }
+      <div className='flex-1'>
+        <Appbar setClick={setClick}/>
         <Body />
       </div>
     </div>
